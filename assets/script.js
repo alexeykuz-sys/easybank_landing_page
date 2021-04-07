ccad661e1b8fd71d17eca00d02df11ec1b26e459
alexeykuz-sys <alexeykuz@gmail.com>
2021-04-07 07:45:37 +0100

    beautified navbar

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,21 @@
-const hamburgerBtn = document.getElementById("hamburger-icon");
 const modal = document.getElementById("modal");
-const closeBtn = document.getElementById("cross-icon");
+const closeBtn = document.getElementById("close-icon");
+const hamburgerBtn = document.getElementById("hamburger-icon");
 const navBarToggler = document.querySelector('.navbar-toggler')
 
 
 function hamburgerHandler() {
   modal.classList.add("show");
   closeBtn.classList.add('show')
-  navBarToggler.classList.remove('show')
+  hamburgerBtn.classList.remove('show')
+  closeBtn.classList.add('show')
 }
 hamburgerBtn.addEventListener("click", hamburgerHandler);
 
 function modalClose() {
   modal.classList.remove("show");
   closeBtn.classList.remove('show')
-  hamburgerBtn.classList.a('show')
+  hamburgerBtn.classList.add('show')
 }
 
 closeBtn.addEventListener("click", modalClose);
