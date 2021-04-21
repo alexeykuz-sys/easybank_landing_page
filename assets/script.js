f11ab30b437d757752d8865ee69807c7d46f54d5
alexeykuz-sys <alexeykuz@gmail.com>
2021-04-21 11:51:57 +0100

    fixed hero image

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,31 @@ function modalClose() {
 }
 
 closeBtn.addEventListener("click", modalClose);
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+const appearOptions = {
+  threshold: .2,
+  rootMargin: '50px 10px -50px -10px' 
+}
+
+const appearOnScroll = new IntersectionObserver((entry, appearOnScroll) => {
+  entry.forEach(entry=>{
+    if(!entry.isIntersecting){
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+
+faders.forEach(fader =>{
+  appearOnScroll.observe(fader);
+});
+
+
+sliders.forEach(slider =>{
+  appearOnScroll.observe(slider);
+})
